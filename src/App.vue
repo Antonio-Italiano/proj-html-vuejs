433e99579f83f57d3760ee2ebc5e9e8041389eed
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-31 10:54:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'EsportGames',
+  data() { return },
+}
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+// import './style.css'
+import App from './App.vue'
+// BOOTSTRAP 
+import 'bootstrap/dist/css/bootstrap.min.css';
+// IMPORT FONTAWESOME
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faStar, farStar, faMagnifyingGlass);
+
+const app = createApp(App)
+// GLOBAL COMPONENTS 
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
+
+
