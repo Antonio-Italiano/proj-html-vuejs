4cb451c5716e8f6b56bd8ade8a8adee391c1618b
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-31 11:11:32 +0100

    created SectionJumbotron

---
+++ src/components/SectionJumbotron.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'SectionJumbotron',
+    data() { return },
+}
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
