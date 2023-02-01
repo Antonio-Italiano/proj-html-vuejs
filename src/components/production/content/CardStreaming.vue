300cac64a3fd7e6677b4b519685e9056330e5923
Antonio-Italiano <antoitalia221@gmail.com>
2023-02-01 16:17:07 +0100

    created ContentLiveStreaming & CardStreaming

---
+++ src/components/production/content/CardStreaming.vue
@@ -0,0 +1,77 @@
+<script>
+import LiveIcon from '../generics/LiveIcon.vue'
+import PlayBtn from '../generics/PlayBtn.vue';
+export default {
+    name: 'CardStreaming',
+    components: { LiveIcon, PlayBtn }
+
+}
+</script>
+
+<template>
+    <figure class="carde align-items-start">
+        <img src="../../../assets/img/video-bg-2.png" alt="Streaming">
+        <!-- POSITION ABOVE  -->
+        <div class="live">
+            <live-icon></live-icon>
+        </div>
+        <div class="play">
+            <play-btn></play-btn>
+        </div>
+        <div class="visual d-flex text-white">
+            <div class="d-flex me-5">
+                <div class="number me-2">356599</div>
+                <p>Viewers</p>
+            </div>
+            <div class="d-flex">
+                <div class="number me-2">23599</div>
+                <p>Likes</p>
+            </div>
+        </div>
+    </figure>
+</template>
+
+<style lang="scss" scoped>
+.visual {
+    position: absolute;
+    bottom: 20px;
+    left: 40px;
+
+    .number {
+        color: #94CB53;
+    }
+}
+
+.live {
+    position: absolute;
+    top: 40px;
+    right: 40px;
+}
+
+.play {
+    position: absolute;
+    top: 45%;
+    right: 50%;
+    transform: translate(50%);
+    transform: translateX(35%);
+    scale: 1.3;
+}
+
+.carde {
+    position: relative;
+    height: 500px;
+    border-radius: 20px;
+    background-image: url(../../../assets/img/video-bg.png);
+    background-position: top;
+    background-size: 100%;
+    padding-top: 40px;
+
+    img {
+        max-width: 100%;
+        height: 460px;
+        display: block;
+        border: 1px solid transparent;
+        border-radius: 0 0 20px 20px;
+    }
+}
+</style>
---
+++ src/components/production/content/ContentLiveStreaming.vue
@@ -0,0 +1,34 @@
+<script>
+import BaseHeading from '../generics/BaseHeading.vue';
+import CardStreaming from './CardStreaming.vue';
+export default {
+    name: 'ContentLiveStreaming',
+    components: { BaseHeading, CardStreaming }
+}
+</script>
+
+<template>
+    <div class="container-custom pt-4">
+        <div class="row justify-content-center">
+            <div class="col-7 text-center my-4">
+                <base-heading informative="Live Streming" title="Watch The Streams"
+                    text="Sed ut perspiciatis unde omnis iste unde omnis iste natus error sit voluptatem accusantium doloremque laudantium"></base-heading>
+            </div>
+            <div class="col-10">
+                <card-streaming></card-streaming>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.col-10 {
+    filter: grayscale(.3);
+
+}
+
+.col-10:hover {
+    filter: grayscale(0);
+
+}
+</style>
