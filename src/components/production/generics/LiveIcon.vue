fabc7dc0cf8444843a3b5e958c612720dae78ccb
Antonio-Italiano <antoitalia221@gmail.com>
2023-02-01 15:52:19 +0100

    created PlayBtn & LiveIcon

---
+++ src/components/production/generics/LiveIcon.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'LiveIcon',
+
+}
+</script>
+
+<template>
+    <span class="icon d-flex align-items-center">
+        <img src="../../../assets/icon/live-icon.png" alt="live Icon">
+        <p class="m-0 ms-1">Live</p>
+    </span>
+</template>
+
+<style lang="scss" scoped>
+.icon {
+    width: fit-content;
+    background-color: #FD9A2E;
+    padding: 6px 25px;
+    border-radius: 20px;
+}
+</style>
---
+++ src/components/production/generics/PlayBtn.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'PlayBtn',
+
+}
+</script>
+
+<template>
+    <div class="border-customs ms-4">
+        <a href="#" class=" btn-play d-flex align-items-center justify-content-center">
+            <img src="../../../assets/icon/play-icon.png" alt="play">
+        </a>
+    </div>
+</template>
+
+<style lang="scss">
+.border-customs {
+    width: fit-content;
+    padding: 5px 0;
+    border-radius: 50%;
+    border: 1px solid #08CB7B;
+
+    .btn-play {
+        height: 40px;
+        width: 40px;
+        margin: 2px 7px;
+        border-radius: 50%;
+        background-color: #fff;
+    }
+}
+</style>
