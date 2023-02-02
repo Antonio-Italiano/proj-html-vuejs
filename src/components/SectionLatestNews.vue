b9cfbe81583d32d7c82cfaf179921a7548a98640
Antonio-Italiano <antoitalia221@gmail.com>
2023-02-02 14:24:55 +0100

    created SectionLatestNews

---
+++ src/components/SectionLatestNews.vue
@@ -0,0 +1,20 @@
+<script>
+import ContentLatestNews from './production/content/ContentLatestNews.vue';
+export default {
+    name: 'SectionLatestNews',
+    components: { ContentLatestNews },
+}
+</script>
+
+<template>
+    <section id="section-latest-news">
+        <content-latest-news></content-latest-news>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+#section-latest-news {
+    height: 100vh;
+    background-color: black;
+}
+</style>
