70bba235cc67c6f0330b17eabf8b9b44a39a3290
Antonio-Italiano <antoitalia221@gmail.com>
2023-02-02 10:06:04 +0100

    banner created and added to the section

--- src/components/SectionFeedback.vue
+++ src/components/SectionFeedback.vue
@@ -1,24 +1,28 @@
 <script>
 import ContentFeedback from './production/content/ContentFeedback.vue';
+import Banner from './production/content/Banner.vue';
 export default {
     name: 'SectionFeedback',
-    components: { ContentFeedback },
+    components: { ContentFeedback, Banner },
 }
 </script>
 
 <template>
     <section id="feedback" class="py-5">
         <div class="container-custom ">
-            <div class="row justify-content-center">
+            <div class="row justify-content-center my-5">
                 <content-feedback></content-feedback>
             </div>
+        </div>
+        <div class="container my-5">
+            <banner></banner>
         </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
 #feedback {
-    height: 70vh;
+    min-height: 70vh;
     background-color: black;
 }
 </style>
---
+++ src/components/production/content/Banner.vue
@@ -0,0 +1,74 @@
+<script>
+export default {
+    name: 'Banner',
+}
+</script>
+
+<template>
+    <div class="banner my5">
+        <figure class="h-100 w-100 text-white text-center">
+            <img src="../../../assets/img/banner-item-bg.png" alt="" class="h-100 w-100">
+            <div class="up-left">
+                <img src="../../../assets/img/banner-item-obj1.png" alt="Obj1">
+            </div>
+            <div class="up-center">
+                <h4>Make Offer</h4>
+                <h1>Get 70% Offer Discount</h1>
+                <a href="#" class="btn px-5 py-2 mt-4 text-white">Get Offers</a>
+            </div>
+            <div class="up-right d-flex align-items-end">
+                <img src="../../../assets/img/banner-item-obj2.png" alt="Obj2">
+            </div>
+        </figure>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.banner {
+    height: 50vh;
+
+    figure {
+        position: relative;
+    }
+
+    .up-left {
+        height: 80%;
+        position: absolute;
+        left: 10px;
+        bottom: 0;
+        overflow: hidden;
+
+        img {
+            width: 330px;
+        }
+    }
+
+    .up-center {
+        position: absolute;
+        top: 30%;
+        left: 50%;
+        transform: translate(-50%);
+
+        h1 {
+            font-size: 55px;
+        }
+
+        .btn {
+            background-color: #1A8F3E;
+            border-radius: 30px;
+        }
+    }
+
+    .up-right {
+        height: 65%;
+        position: absolute;
+        right: 10px;
+        top: 0;
+        overflow: hidden;
+
+        img {
+            width: 260px;
+        }
+    }
+}
+</style>
